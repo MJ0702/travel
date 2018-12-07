ff6bfedd3137cf4ec3e16013aa7f4b1b1fb29566
MJ0702 <781082627@qq.com>
2018-12-07 23:53:15 +0800

    add icons

---
+++ travel/src/pages/home/components/Icons.vue
@@ -0,0 +1,124 @@
+<template>
+    <div class="icons">
+      <swiper>
+        <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+        </swiper-slide>
+      </swiper>
+    </div>
+</template>
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+          desc: '自然风光'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+          desc: '名胜古迹'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b6/610ab63cfbecd302.png',
+          desc: '祈福许愿'
+        },
+        {
+          id: '0005',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '必游榜单'
+        },
+        {
+          id: '0006',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/57/1e29afd06f881102.png',
+          desc: '普陀山'
+        },
+        {
+          id: '0007',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/8c/47630407f70e8302.png',
+          desc: '横店影视城'
+        },
+        {
+          id: '0008',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b8/c5dcdb58deec2402.png',
+          desc: '虎踞峡'
+        },
+        {
+          id: '0009',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+          desc: '东部华侨城'
+        },
+        {
+          id: '0010',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/80/416c6ab3368d1f02.png',
+          desc: '全部玩乐'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl';
+  @import '~styles/mixins.styl';
+  .icons >>> .swiper-container
+    height:0
+    padding-bottom:50%
+    .icon
+      height:0
+      position:relative
+      overflow:hidden
+      float:left
+      width:25%
+      padding-bottom:25%
+      .icon-img
+        position:absolute
+        top:0
+        left:0
+        right:0
+        bottom:.44rem
+        box-sizing:border-box
+        padding:.1rem
+        .icon-img-content
+          display:block
+          height:100%
+          margin:0 auto
+      .icon-desc
+        position:absolute
+        left:0
+        right:0
+        bottom:0
+        height:0.44rem
+        line-height:.44rem
+        text-align:center
+        color:$darkTextColor
+        ellipsis()
+</style>
