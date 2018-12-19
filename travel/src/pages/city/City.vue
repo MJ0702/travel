0a6f7d120a5951f30209cadcbde0e7b882eed796
MJ0702 <781082627@qq.com>
2018-12-19 23:53:31 +0800

    city drag

--- travel/src/pages/city/City.vue
+++ travel/src/pages/city/City.vue
@@ -2,8 +2,16 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :cities="cities" :hot="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+    <city-list
+    :cities="cities"
+    :hot="hotCities"
+    :letter="letter"
+    ></city-list>
+    <city-alphabet
+    :cities="cities"
+    @change="handleletterChange"
+    >
+    </city-alphabet>
   </div>
 </template>
 
@@ -24,7 +32,8 @@ export default {
   data () {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   methods: {
@@ -33,13 +42,16 @@ export default {
         .then(this.handleGetCityInfoSucc)
     },
     handleGetCityInfoSucc (res) {
-      console.log(res)
+      // console.log(res)
       res = res.data
       if (res.ret && res.data) {
         const data = res.data
         this.cities = data.cities
         this.hotCities = data.hotCities
       }
+    },
+    handleletterChange (letter) {
+      this.letter = letter
     }
   },
   mounted () {
